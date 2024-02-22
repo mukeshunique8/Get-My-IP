61082db2934c04bbabe2371bd1e6bc5d2de8b0cd
mukeshunique8 <muthu888focus@gmail.com>
2024-02-22 12:31:58 +0530

    FIX : BASE URL ADDED

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  // base: 'http://127.0.0.1:5500/index.html',
+  base: '/Get-My-IP/',
   //   build: {
   //     rollupOptions: {
   //       input: {
