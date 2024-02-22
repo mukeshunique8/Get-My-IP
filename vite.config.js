1f639c24197061add10e3bf6ea84c4b2202d4b6c
Muthu Kumaran M <121846128+mukeshunique8@users.noreply.github.com>
2024-02-22 13:05:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  // base: '/Get-My-IP/',
+  base: '/Get-My-IP/',
   //   build: {
   //     rollupOptions: {
   //       input: {
